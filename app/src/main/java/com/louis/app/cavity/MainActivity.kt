00fb6302e4d44e8d6195c96cd9579b2454d2f6c3
bailly64u <louis.bailly1@etu.univ-lorraine.fr>
2020-08-12 22:34:02 +0200

    Initial commit

---
+++ app/src/main/java/com/louis/app/cavity/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.louis.app.cavity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
