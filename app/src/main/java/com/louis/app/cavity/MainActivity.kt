4dfa868e95698cf4c9c7508c56f7e38932f648b5
bailly64u <louis.bailly1@etu.univ-lorraine.fr>
2020-08-13 22:14:40 +0200

    Add items for county, tasting & history

--- app/src/main/java/com/louis/app/cavity/MainActivity.kt
+++ app/src/main/java/com/louis/app/cavity/MainActivity.kt
@@ -9,10 +9,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tab = findViewById<CountyScrollableTab>(R.id.tab)
-        tab.addTabs(listOf(
-            "Jan 2018", "Feb 2018", "Mar 2018", "Apr 2018", "May 2018", "Jun 2018", "July 2018",
-            "Aug 2018", "Sep 2018", "Oct 2018", "Nov 2018", "Dec 2018"
-        ))
+//        val tab = findViewById<CountyScrollableTab>(R.id.tab)
+//        tab.addTabs(listOf(
+//            "Jan 2018", "Feb 2018", "Mar 2018", "Apr 2018", "May 2018", "Jun 2018", "July 2018",
+//            "Aug 2018", "Sep 2018", "Oct 2018", "Nov 2018", "Dec 2018"
+//        ))
     }
 }
