87938cd8d8e20f71b14a991a6c95fadb420c66f6
Bailly Louis <louis.bailly@quarks.pro>
2020-09-15 14:07:54 +0200

    Search layout

---
+++ app/src/main/java/com/louis/app/cavity/ui/search/FragmentSearch.kt
@@ -0,0 +1,25 @@
+package com.louis.app.cavity.ui.search
+
+import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.louis.app.cavity.R
+import com.louis.app.cavity.databinding.FragmentSearchBinding
+import com.louis.app.cavity.ui.ActivityMain
+
+class FragmentSearch : Fragment(R.layout.fragment_search) {
+    private lateinit var binding : FragmentSearchBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentSearchBinding.bind(view)
+
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.bottomSheet)
+        bottomSheetBehavior.isHideable = false
+
+        (activity as ActivityMain).dropToolbarShadow()
+    }
+}
