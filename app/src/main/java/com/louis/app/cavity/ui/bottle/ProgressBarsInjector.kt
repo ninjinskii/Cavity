674a202e8c5b2310fb14c73210aa29a082230154
bailly64u <louis.bailly1@etu.univ-lorraine.fr>
2020-10-06 23:12:32 +0200

    Working on BottleDetails layout

---
+++ app/src/main/java/com/louis/app/cavity/ui/bottle/ProgressBarsInjector.kt
@@ -0,0 +1,6 @@
+package com.louis.app.cavity.ui.bottle
+
+// Responsible for creating a bar to show grapes
+class ProgressBarsInjector() {
+    //progressBar.setProgressTintList(ColorStateList.valueOf(Color.RED)); // change color
+}
