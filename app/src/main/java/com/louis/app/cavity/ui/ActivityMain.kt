47a65fb63d6c89389deae69481a2668859f2e61e
Bailly Louis <louis.bailly@quarks.pro>
2020-11-22 12:24:02 +0100

    Add custom back navigation for Drawer

--- app/src/main/java/com/louis/app/cavity/ui/ActivityMain.kt
+++ app/src/main/java/com/louis/app/cavity/ui/ActivityMain.kt
@@ -1,26 +1,17 @@
 package com.louis.app.cavity.ui
 
-import android.animation.AnimatorInflater
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
-import androidx.fragment.app.activityViewModels
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
-import com.louis.app.cavity.R
 import com.louis.app.cavity.databinding.ActivityMainBinding
-import com.louis.app.cavity.ui.addbottle.AddBottleViewModel
-import com.louis.app.cavity.util.setVisible
 import com.louis.app.cavity.util.showSnackbar
 
 class ActivityMain : AppCompatActivity(), SnackbarProvider {
     lateinit var navView: NavigationView
     lateinit var drawer: DrawerLayout
     private lateinit var binding: ActivityMainBinding
-    private var isToolbarShadowShown = true
-    private var hasCustomToolbar = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,6 +23,14 @@ class ActivityMain : AppCompatActivity(), SnackbarProvider {
     private fun exposeNavigationStuff() {
         navView = binding.navView
         drawer = binding.drawer
+    }
+
+    override fun onBackPressed() {
+        if (binding.drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
     override fun onShowSnackbarRequested(stringRes: Int) {
