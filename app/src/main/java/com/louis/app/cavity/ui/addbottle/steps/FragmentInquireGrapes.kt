8cb43d1c7430390f51ed5c2703eaa1c6c101e8f1
Bailly Louis <louis.bailly@quarks.pro>
2020-12-02 18:51:54 +0100

    Re implement add grape dialog

--- app/src/main/java/com/louis/app/cavity/ui/addbottle/steps/FragmentInquireGrapes.kt
+++ app/src/main/java/com/louis/app/cavity/ui/addbottle/steps/FragmentInquireGrapes.kt
@@ -9,11 +9,14 @@ import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.louis.app.cavity.R
+import com.louis.app.cavity.databinding.DialogAddGrapeBinding
 import com.louis.app.cavity.databinding.FragmentInquireGrapesBinding
 import com.louis.app.cavity.model.Grape
-import com.louis.app.cavity.model.relation.QuantifiedGrapeAndGrape
 import com.louis.app.cavity.ui.addbottle.AddBottleViewModel
-import com.louis.app.cavity.util.*
+import com.louis.app.cavity.util.hideKeyboard
+import com.louis.app.cavity.util.setVisible
+import com.louis.app.cavity.util.showKeyboard
+import com.louis.app.cavity.util.showSnackbar
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
@@ -71,7 +74,7 @@ class FragmentInquireGrapes : Fragment(R.layout.fragment_inquire_grapes) {
                 val bool = checkableGrapes.map { it.isChecked }.toBooleanArray()
 
                 MaterialAlertDialogBuilder(requireContext())
-                    .setTitle(R.string.add_grapes)
+                    .setTitle(R.string.select_grapes)
                     .setMultiChoiceItems(names, bool) { _, pos, checked ->
                         copy[pos].isChecked = checked
                     }
@@ -86,11 +89,27 @@ class FragmentInquireGrapes : Fragment(R.layout.fragment_inquire_grapes) {
     }
 
     private fun setListeners() {
-        binding.buttonAddGrape.setOnClickListener { grapeViewModel.requestGrapeDialog() }
+        binding.buttonSelectGrape.setOnClickListener { grapeViewModel.requestGrapeDialog() }
 
-        binding.buttonCreateGrape.setOnClickListener {
-            grapeViewModel.insertGrape(Grape(grapeId = 0, name = binding.grapeName.text.toString()))
-        }
+        binding.buttonAddGrape.setOnClickListener { showAddGrapeDialog() }
+    }
+
+    private fun showAddGrapeDialog() {
+        val dialogBinding = DialogAddGrapeBinding.inflate(layoutInflater)
+
+        MaterialAlertDialogBuilder(requireContext())
+            .setTitle(R.string.add_grape)
+            .setNegativeButton(R.string.cancel) { _, _ ->
+            }
+            .setPositiveButton(R.string.submit) { _, _ ->
+                val name = dialogBinding.grapeName.text.toString()
+                grapeViewModel.insertGrape(Grape(grapeId = 0, name))
+            }
+            .setView(dialogBinding.root)
+            .setOnDismissListener { dialogBinding.root.hideKeyboard() }
+            .show()
+
+        dialogBinding.grapeName.post { dialogBinding.grapeName.showKeyboard() }
     }
 
     private fun addGrape(grapeName: String) {
