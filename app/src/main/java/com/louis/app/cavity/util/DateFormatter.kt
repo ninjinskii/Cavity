7ea028d92533601b768b3af82dd0852fdc7bd54c
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-02-17 17:35:49 +0100

    Fix isToday() returning true even for tomorrow timestamp

--- app/src/main/java/com/louis/app/cavity/util/DateFormatter.kt
+++ app/src/main/java/com/louis/app/cavity/util/DateFormatter.kt
@@ -8,6 +8,7 @@ object DateFormatter {
 
     fun formatDate(timestamp: Long?): String {
         return if (timestamp != null && timestamp > 0L) {
+            // TODO: i18n
             val formatter = SimpleDateFormat("dd MMM yyyy", Locale.FRENCH)
             val calendar = Calendar.getInstance()
 
@@ -27,7 +28,7 @@ object DateFormatter {
     fun isToday(timestamp: Long?): Boolean {
         val currentTime = System.currentTimeMillis()
         val startOfDay = roundToDay(currentTime)
-        val endOfDay = startOfDay + DAY_IN_MILLIS
+        val endOfDay = startOfDay + DAY_IN_MILLIS - 1 // -1 to not overlap next day
 
         return timestamp in startOfDay..endOfDay
     }
