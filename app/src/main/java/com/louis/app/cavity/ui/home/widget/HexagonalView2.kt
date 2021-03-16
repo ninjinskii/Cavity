4b0a51a76d59eb3c6ca2642fca10ffab89edbddb
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-03-17 00:27:40 +0100

    Add shadow based on outline

--- app/src/main/java/com/louis/app/cavity/ui/home/widget/HexagonalView2.kt
+++ app/src/main/java/com/louis/app/cavity/ui/home/widget/HexagonalView2.kt
@@ -1,13 +1,19 @@
 package com.louis.app.cavity.ui.home.widget
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Path
+import android.content.res.ColorStateList
+import android.graphics.*
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.RippleDrawable
+import android.graphics.drawable.ShapeDrawable
 import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import android.widget.LinearLayout
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.use
+import com.google.android.material.shape.ShapePath
 import com.louis.app.cavity.R
 import kotlin.math.round
 
@@ -16,15 +22,13 @@ class HexagonalView2 @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ) :
-    ConstraintLayout(context, attrs, defStyleAttr) {
+    LinearLayout(context, attrs, defStyleAttr) {
 
     companion object {
         private const val HEXAGONAL_SQUARE_RATIO = 0.866
     }
 
-    private val path = Path().apply {
-        fillType = Path.FillType.EVEN_ODD
-    }
+    private val path = Path()
 
     private val paint by lazy {
         Paint(Paint.ANTI_ALIAS_FLAG).apply {
@@ -45,6 +49,34 @@ class HexagonalView2 @JvmOverloads constructor(
             .use {
                 isFlat = it.getBoolean(R.styleable.HexagonalView_flat, false)
             }
+
+//        outlineProvider = object : ViewOutlineProvider() {
+//            override fun getOutline(view: View, outline: Outline?) {
+//                if (android.os.Build.VERSION.SDK_INT < android.os.Build.VERSION_CODES.R) {
+//                    outline?.setConvexPath(path)
+//                } else {
+//                    outline?.setPath(path)
+//                }
+//            }
+//        }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        val largerSide = if (isFlat) w.toFloat() else h.toFloat()
+        val smallerSide = if (isFlat) h.toFloat() else w.toFloat()
+
+        path.run {
+            moveTo(0f, largerSide / 2)
+            lineTo(0f, largerSide * 0.25f)
+            lineTo(smallerSide / 2, 0f)
+            lineTo(smallerSide, largerSide * 0.25f)
+            lineTo(smallerSide, largerSide * 0.75f)
+            lineTo(smallerSide / 2, largerSide)
+            lineTo(0f, largerSide * 0.75f)
+            lineTo(0f, largerSide / 2)
+        }
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -69,21 +101,6 @@ class HexagonalView2 @JvmOverloads constructor(
     }
 
     override fun onDraw(canvas: Canvas) {
-        val largerSide = if (isFlat) width.toFloat() else height.toFloat()
-        val smallerSide = if (isFlat) height.toFloat() else width.toFloat()
-
-        path.run {
-            moveTo(0f, largerSide / 2)
-            lineTo(0f, largerSide * 0.25f)
-            lineTo(smallerSide / 2, 0f)
-            lineTo(smallerSide, largerSide * 0.25f)
-            lineTo(smallerSide, largerSide * 0.75f)
-            lineTo(smallerSide / 2, largerSide)
-            lineTo(0f, largerSide * 0.75f)
-            lineTo(0f, largerSide / 2)
-            close()
-        }
-
         canvas.drawPath(path, paint)
     }
 }
