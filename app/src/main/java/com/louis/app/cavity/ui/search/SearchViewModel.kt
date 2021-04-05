f3aa697e07db42f8cd21665fc3584e6598a94d3c
Bailly Louis <louis.bailly@quarks.pro>
2021-04-05 23:45:45 +0200

    Fix weird bottom sheet position

--- app/src/main/java/com/louis/app/cavity/ui/search/SearchViewModel.kt
+++ app/src/main/java/com/louis/app/cavity/ui/search/SearchViewModel.kt
@@ -12,6 +12,7 @@ import com.louis.app.cavity.model.Grape
 import com.louis.app.cavity.model.Review
 import com.louis.app.cavity.model.relation.bottle.BoundedBottle
 import com.louis.app.cavity.ui.search.filters.*
+import com.louis.app.cavity.util.L
 import kotlinx.coroutines.Dispatchers.Default
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
@@ -50,6 +51,7 @@ class SearchViewModel(app: Application) : AndroidViewModel(app) {
         viewModelScope.launch(IO) {
             bottlesAndWine.addAll(repository.getWineAndBottleWithQGrapesAndFReviews())
             _results.postValue(bottlesAndWine)
+            L.v("Post list value from db", "DEBUGGING SEARCH")
         }
     }
 
@@ -71,11 +73,13 @@ class SearchViewModel(app: Application) : AndroidViewModel(app) {
             val filtered = combinedFilters.meetFilters(bottlesAndWine)
             // Deleting 'toList()' seems to introduce a bug sometimes, where the observer is not
             // aware that the data has been changed, the first time you access FragmentSearch
+            L.v("posting value to observer with a list with ${filtered.size} items")
             _results.postValue(filtered)
         }
     }
 
     fun setCountiesFilters(filteredCounties: List<County>) {
+        L.v("counties: $filteredCounties")
         counties = filteredCounties.map { it.id }
 
         val countyFilters: List<WineFilter> = counties.map { FilterCounty(it) }
--- app/src/main/java/com/louis/app/cavity/ui/search/filters/And.kt
+++ app/src/main/java/com/louis/app/cavity/ui/search/filters/And.kt
@@ -3,8 +3,7 @@ package com.louis.app.cavity.ui.search.filters
 import com.louis.app.cavity.model.relation.bottle.BoundedBottle
 
 class And(private val filter: WineFilter, private val otherFilter: WineFilter) : WineFilter {
-    override fun meetFilters(boundedBottle: List<BoundedBottle>):
-            List<BoundedBottle> {
+    override fun meetFilters(boundedBottle: List<BoundedBottle>): List<BoundedBottle> {
         val firstFilter = filter.meetFilters(boundedBottle)
         return otherFilter.meetFilters(firstFilter)
     }
