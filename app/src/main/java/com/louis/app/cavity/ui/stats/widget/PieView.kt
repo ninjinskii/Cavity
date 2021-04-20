560055c59bef107209b2c43511fb41704d31e770
Bailly Louis <louis.bailly@quarks.pro>
2021-04-21 00:30:18 +0200

    Fix wrap content size state for PieView

--- app/src/main/java/com/louis/app/cavity/ui/stats/widget/PieView.kt
+++ app/src/main/java/com/louis/app/cavity/ui/stats/widget/PieView.kt
@@ -59,10 +59,19 @@ class PieView @JvmOverloads constructor(
         centerY = rect.centerY()
     }
 
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val minw: Int = paddingLeft + paddingRight + suggestedMinimumWidth
+        val w: Int = resolveSizeAndState(minw, widthMeasureSpec, 1)
+        val h: Int = resolveSizeAndState(MeasureSpec.getSize(w), heightMeasureSpec, 0)
+
+        setMeasuredDimension(w, h)
+    }
+
+
     override fun onDraw(canvas: Canvas) {
         piePaint.color = Color.BLACK
 
-        canvas.drawCircle(centerX, centerY, pieRadius + STROKE_WIDTH, piePaint)
+        canvas.drawCircle(centerX, centerY, pieRadius - STROKE_WIDTH, piePaint)
 
         var previousAngle = -90f
 
