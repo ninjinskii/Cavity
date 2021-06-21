fa19fb19f3129aa26bb087084fff1e63309c60ab
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-06-21 23:47:10 +0200

    Prepare FragmentInquireTastingInfo

---
+++ app/src/main/java/com/louis/app/cavity/ui/tasting/FragmentInquireTastingInfo.kt
@@ -0,0 +1,7 @@
+package com.louis.app.cavity.ui.tasting
+
+import androidx.fragment.app.Fragment
+import com.louis.app.cavity.R
+
+class FragmentInquireTastingInfo : Fragment(R.layout.fragment_inquire_tasting_info) {
+}
