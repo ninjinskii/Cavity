6f07dadf508ec47aeedc49c62e6819337f46aadd
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-08-02 00:23:36 +0200

    Add tasting bottle item layout

--- app/src/main/java/com/louis/app/cavity/ui/addtasting/FragmentAddTasting.kt
+++ app/src/main/java/com/louis/app/cavity/ui/addtasting/FragmentAddTasting.kt
@@ -14,6 +14,7 @@ class FragmentAddTasting : Stepper() {
     override val steps = setOf(
         FragmentInquireTastingInfo(),
         FragmentSearch().apply { arguments = bundleOf(PICK_MODE to true) },
+        FragmentInquireSchedule()
     )
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/louis/app/cavity/ui/addtasting/FragmentInquireSchedule.kt
@@ -0,0 +1,8 @@
+package com.louis.app.cavity.ui.addtasting
+
+import com.louis.app.cavity.R
+import com.louis.app.cavity.ui.stepper.Step
+
+class FragmentInquireSchedule : Step(R.layout.fragment_inquire_schedule) {
+
+}
