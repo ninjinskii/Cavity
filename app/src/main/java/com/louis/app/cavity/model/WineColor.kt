c43d906337129bf50bc77523498aa56a5e635fe9
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2021-12-16 11:55:04 +0100

    Remove temperature related stuff

--- app/src/main/java/com/louis/app/cavity/model/WineColor.kt
+++ app/src/main/java/com/louis/app/cavity/model/WineColor.kt
@@ -3,9 +3,9 @@ package com.louis.app.cavity.model
 import androidx.annotation.ColorRes
 import com.louis.app.cavity.R
 
-enum class WineColor(@ColorRes val colorRes: Int, val defaultTemperature: Temperature) {
-    RED(R.color.wine_red, Temperature.Celsius(15)),
-    WHITE(R.color.wine_white, Temperature.Celsius(10)),
-    SWEET(R.color.wine_sweet, Temperature.Celsius(5)),
-    ROSE(R.color.wine_rose, Temperature.Celsius(6))
+enum class WineColor(@ColorRes val colorRes: Int) {
+    RED(R.color.wine_red),
+    WHITE(R.color.wine_white),
+    SWEET(R.color.wine_sweet),
+    ROSE(R.color.wine_rose)
 }
