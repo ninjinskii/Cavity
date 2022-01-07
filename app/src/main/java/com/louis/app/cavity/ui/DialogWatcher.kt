9af69d181f75df87a6ddcdbc94cd46c8693fed0b
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-01-07 18:03:26 +0100

    Remove unused interface

--- app/src/main/java/com/louis/app/cavity/ui/DialogWatcher.kt
+++
@@ -1,8 +0,0 @@
-package com.louis.app.cavity.ui
-
-/**
- * Interface responsible to close opened dialogs if any when navigating to avoid monkey like crashes
- */
-interface DialogWatcher {
-    fun closeMaybeOpenedDialog()
-}
