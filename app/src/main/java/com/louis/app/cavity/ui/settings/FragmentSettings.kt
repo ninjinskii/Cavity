da8995bc7c37706a9bdb5e52d7bfebd6350bd514
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2022-02-07 12:40:04 +0100

    Implement base setting layout stuff

--- app/src/main/java/com/louis/app/cavity/ui/settings/FragmentSettings.kt
+++ app/src/main/java/com/louis/app/cavity/ui/settings/FragmentSettings.kt
@@ -2,8 +2,11 @@ package com.louis.app.cavity.ui.settings
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.button.MaterialButton
 import com.louis.app.cavity.R
 import com.louis.app.cavity.databinding.FragmentSettingsBinding
 import com.louis.app.cavity.util.TransitionHelper
@@ -14,6 +17,7 @@ class FragmentSettings : Fragment(R.layout.fragment_settings) {
     private var _binding: FragmentSettingsBinding? = null
     private val binding get() = _binding!!
     private val settingsViewModel: SettingsViewModel by viewModels()
+    private var clickCount = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +35,7 @@ class FragmentSettings : Fragment(R.layout.fragment_settings) {
 
         observe()
         setListeners()
+        setupCurrencyButtons()
     }
 
     private fun observe() {
@@ -42,10 +47,54 @@ class FragmentSettings : Fragment(R.layout.fragment_settings) {
     }
 
     private fun setListeners() {
+        binding.toggleSkew.apply {
+            thumbDrawable = ResourcesCompat.getDrawable(
+                resources,
+                R.drawable.switch_thumb,
+                requireContext().theme
+            )
+
+            setOnCheckedChangeListener { _, isChecked ->
+                clickCount++
+            }
+
+            setOnLongClickListener {
+                if (clickCount == 10) {
+                    val externalDir = requireContext().getExternalFilesDir(null)!!.path
+                    settingsViewModel.importDbFromExternalDir(externalDir)
+                }
+
+                true
+            }
+        }
+
+        binding.skewBottle.apply {
+            setOnClickListener {
+                binding.toggleSkew.toggle()
+            }
+
+            setOnLongClickListener {
+                if (clickCount == 10) {
+                    val externalDir = requireContext().getExternalFilesDir(null)!!.path
+                    settingsViewModel.importDbFromExternalDir(externalDir)
+                }
+
+                true
+            }
+
 //        binding.buttonImportDb.setOnClickListener {
 //            val externalDir = requireContext().getExternalFilesDir(null)!!.path
 //            settingsViewModel.importDbFromExternalDir(externalDir)
 //        }
+        }
+    }
+
+    private fun setupCurrencyButtons() {
+        val currencies = resources.getStringArray(R.array.currencies)
+
+        binding.rbGroupCurrency.children.forEachIndexed { index, view ->
+            (view as MaterialButton).text = currencies[index]
+        }
     }
 
     override fun onDestroy() {
