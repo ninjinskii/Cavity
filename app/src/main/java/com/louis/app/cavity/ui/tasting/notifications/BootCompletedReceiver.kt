5ef47446a798bb50094e2ab183c3d71e5f161396
ninjinski <louis.bailly.pro@gmail.com>
2023-02-25 17:48:10 +0100

    Remove coroutine GlobalScope usage

--- app/src/main/java/com/louis/app/cavity/ui/tasting/notifications/BootCompletedReceiver.kt
+++ app/src/main/java/com/louis/app/cavity/ui/tasting/notifications/BootCompletedReceiver.kt
@@ -5,8 +5,9 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import com.louis.app.cavity.db.WineRepository
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 
 class BootCompletedReceiver : BroadcastReceiver() {
@@ -14,7 +15,7 @@ class BootCompletedReceiver : BroadcastReceiver() {
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
             val repository = WineRepository.getInstance(context.applicationContext as Application)
 
-            GlobalScope.launch(IO) {
+            CoroutineScope(SupervisorJob()).launch(IO) {
                 repository.getAllTastingsNotLive().forEach {
                     TastingAlarmScheduler.scheduleTastingAlarm(context, it)
                 }
