693f88b3164960b0ec42e893efef6e354a6869e7
ninjinski <louis.bailly@edu.univ-fcomte.fr>
2024-01-13 21:15:49 +0100

    1.5.4

--- app/src/main/java/com/louis/app/cavity/db/CavityDatabase.kt
+++ app/src/main/java/com/louis/app/cavity/db/CavityDatabase.kt
@@ -50,38 +50,40 @@ abstract class CavityDatabase : RoomDatabase() {
 
         private val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("BEGIN TRANSACTION;")
+                try {
+                    database.beginTransaction()
+                    database.execSQL(
+                        "CREATE TABLE `bottle_temp` " +
+                            "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
+                            " `wine_id` INTEGER NOT NULL," +
+                            " `vintage` INTEGER NOT NULL," +
+                            " `apogee` INTEGER," +
+                            " `is_favorite` INTEGER NOT NULL," +
+                            " `price` REAL NOT NULL," +
+                            " `currency` TEXT NOT NULL," +
+                            " `other_info` TEXT NOT NULL," +
+                            " `buy_location` TEXT NOT NULL," +
+                            " `buy_date` INTEGER NOT NULL," +
+                            " `tasting_taste_comment` TEXT NOT NULL," +
+                            " `bottle_size` TEXT NOT NULL," +
+                            " `pdf_path` TEXT NOT NULL," +
+                            " `consumed` INTEGER NOT NULL," +
+                            " `tasting_id` INTEGER," +
+                            " FOREIGN KEY(`wine_id`) REFERENCES `wine`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE );"
+                    )
 
-                database.execSQL("CREATE TABLE `bottle_temp` " +
-                    "(`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL," +
-                    " `wine_id` INTEGER NOT NULL," +
-                    " `vintage` INTEGER NOT NULL," +
-                    " `apogee` INTEGER," +
-                    " `is_favorite` INTEGER NOT NULL," +
-                    " `price` REAL NOT NULL," +
-                    " `currency` TEXT NOT NULL," +
-                    " `other_info` TEXT NOT NULL," +
-                    " `buy_location` TEXT NOT NULL," +
-                    " `buy_date` INTEGER NOT NULL," +
-                    " `tasting_taste_comment` TEXT NOT NULL," +
-                    " `bottle_size` TEXT NOT NULL," +
-                    " `pdf_path` TEXT NOT NULL," +
-                    " `consumed` INTEGER NOT NULL," +
-                    " `tasting_id` INTEGER," +
-                    " FOREIGN KEY(`wine_id`) REFERENCES `wine`(`id`) ON UPDATE NO ACTION ON DELETE CASCADE );"
-                )
+                    database.execSQL("CREATE INDEX index_bottle_wine_id ON bottle_temp(wine_id ASC);")
 
-                database.execSQL("CREATE INDEX index_bottle_wine_id ON bottle_temp(wine_id ASC);")
+                    database.execSQL("CREATE INDEX index_bottle_tasting_id ON bottle_temp(tasting_id ASC);")
 
-                database.execSQL("CREATE INDEX index_bottle_tasting_id ON bottle_temp(tasting_id ASC);")
+                    database.execSQL("INSERT INTO bottle_temp(id, wine_id, vintage, apogee, is_favorite, price, currency, other_info, buy_location, buy_date, tasting_taste_comment, bottle_size, pdf_path, consumed, tasting_id) SELECT * FROM bottle;")
 
-                database.execSQL("INSERT INTO bottle_temp(id, wine_id, vintage, apogee, is_favorite, price, currency, other_info, buy_location, buy_date, tasting_taste_comment, bottle_size, pdf_path, consumed, tasting_id) SELECT * FROM bottle;")
+                    database.execSQL("DROP TABLE bottle;")
 
-                database.execSQL("DROP TABLE bottle;")
-
-                database.execSQL("ALTER TABLE bottle_temp RENAME TO bottle;")
-
-                database.execSQL("COMMIT;")
+                    database.execSQL("ALTER TABLE bottle_temp RENAME TO bottle;")
+                } finally {
+                    database.endTransaction()
+                }
             }
         }
 
