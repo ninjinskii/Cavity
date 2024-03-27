1392e2dad690b71550eb4fb8294a12bb6b5053d3
ninjinski <louis.bailly.pro@gmail.com>
2024-03-27 20:05:17 +0100

    chore: add db migration test fundation

---
+++ app/src/test/kotlin/RandomTest.kt
@@ -0,0 +1,11 @@
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class EmailValidatorTest {
+    @Test fun emailValidator_CorrectEmailSimple_ReturnsTrue() {
+        assertTrue(true)
+    }
+
+}
