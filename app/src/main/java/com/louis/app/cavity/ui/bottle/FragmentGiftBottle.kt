243eb282103c6fd91ae7add34f3c32e62b40c2d7
ninjinski <>
2025-07-04 18:52:22 +0200

    fix: prevent date picker memory leak

--- app/src/main/java/com/louis/app/cavity/ui/bottle/FragmentGiftBottle.kt
+++ app/src/main/java/com/louis/app/cavity/ui/bottle/FragmentGiftBottle.kt
@@ -25,6 +25,7 @@ import com.louis.app.cavity.util.prepareWindowInsets
 
 class FragmentGiftBottle : Fragment(R.layout.fragment_gift_bottle) {
     private lateinit var snackbarProvider: SnackbarProvider
+    private var datePicker: DatePicker? = null
     private var _binding: FragmentGiftBottleBinding? = null
     private val binding get() = _binding!!
     private val addItemViewModel: AddItemViewModel by activityViewModels()
@@ -67,7 +68,7 @@ class FragmentGiftBottle : Fragment(R.layout.fragment_gift_bottle) {
     private fun initDatePicker() {
         val title = getString(R.string.consume_date)
 
-        DatePicker(
+        datePicker = DatePicker(
             childFragmentManager,
             binding.giftDateLayout,
             title,
@@ -146,6 +147,7 @@ class FragmentGiftBottle : Fragment(R.layout.fragment_gift_bottle) {
 
     override fun onDestroy() {
         super.onDestroy()
+        datePicker?.dispose()
         _binding = null
     }
 }
