68535eb26e51c9d1f225dc403cb7d2ea6fffe8e9
Ninjinski <31962617+ninjinskii@users.noreply.github.com>
2026-01-06 14:50:19 +0000

    feat(home): only blur lines of text instead of all TextView (#98)
    * feat(home): add per line blur effect
    
    * feat(home): simplify per line blur effect
    
    * feat(home): always use line boundaries to draw blur instead of view boundaries
    
    * fix(home): reset item wine maxLine to 2
    
    * fix(home): restore wine naming visibility
    
    ---------
    
    Co-authored-by: n <n>

--- app/src/main/java/com/louis/app/cavity/ui/home/widget/EffectImageView.kt
+++ app/src/main/java/com/louis/app/cavity/ui/home/widget/EffectImageView.kt
@@ -7,7 +7,7 @@ import android.graphics.RenderNode
 import android.graphics.Shader
 import android.os.Build
 import android.util.AttributeSet
-import android.view.View
+import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.widget.AppCompatImageView
 
@@ -24,10 +24,18 @@ class EffectImageView @JvmOverloads constructor(
     @RequiresApi(Build.VERSION_CODES.S)
     private val blur = RenderEffect.createBlurEffect(30f, 30f, Shader.TileMode.CLAMP)
 
-    private val targets = mutableListOf<Pair<View, RenderNode>>()
+    private val targets = mutableListOf<Pair<TextView, RenderNode>>()
+
+    // Pre allocated render nodes to use with lines if targeting mutli-line text views.
+    @RequiresApi(Build.VERSION_CODES.Q)
+    private val additionalRenderNodes = listOf(
+        RenderNode("blur-textviewline-1"),
+        RenderNode("blur-textviewline-2"),
+        RenderNode("blur-textviewline-3")
+    )
 
     @RequiresApi(Build.VERSION_CODES.S)
-    fun setTargets(views: List<View>) {
+    fun setTargets(views: List<TextView>) {
         this.targets.clear()
         this.targets.addAll(views.map { it to RenderNode("blur-${it.id}") })
     }
@@ -47,17 +55,33 @@ class EffectImageView @JvmOverloads constructor(
         // Image is now drawn as usual, now draw blur behind targeted views
         targets.forEach { pair ->
             val (view, renderNode) = pair
+            val layout = view.layout
+            val textViewReady = view.layout != null && view.layout.lineCount > 0
 
-            with(renderNode) {
-                setRenderEffect(blur)
-                setPosition(0, 0, view.width, view.height)
-                translationX = view.left.toFloat()
-                translationY = view.top.toFloat()
-                val blurCanvas = beginRecording()
-                blurCanvas.translate(-view.left.toFloat(), -view.top.toFloat())
-                blurCanvas.drawRenderNode(underlyingContentNode)
-                endRecording()
-                canvas.drawRenderNode(this)
+            if (textViewReady) {
+                var i = 0
+
+                for (line in 0..< layout.lineCount) {
+                    val renderNode = if (i++ == 0) renderNode else additionalRenderNodes[i]
+                    val lineStart = view.left + layout.getLineLeft(line)
+                    val lineEnd = view.left + layout.getLineRight(line)
+                    val lineBottom = view.top + layout.getLineBottom(line)
+                    val lineTop = view.top + layout.getLineTop(line)
+                    val right = lineEnd - lineStart
+                    val bottom = lineBottom - lineTop
+
+                    with(renderNode) {
+                        setRenderEffect(blur)
+                        setPosition(0, 0, right.toInt(), bottom)
+                        translationX = lineStart
+                        translationY = lineTop.toFloat()
+                        val blurCanvas = beginRecording()
+                        blurCanvas.translate(-lineStart, -lineTop.toFloat())
+                        blurCanvas.drawRenderNode(underlyingContentNode)
+                        endRecording()
+                        canvas.drawRenderNode(this)
+                    }
+                }
             }
         }
     }
