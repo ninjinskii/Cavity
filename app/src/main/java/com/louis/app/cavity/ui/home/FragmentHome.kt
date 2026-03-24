b4a5e5fe4acfee82d08ea27b2e9390e827936c4b
n <n>
2026-03-24 14:05:33 +0100

    chore: update toolbar setup for every fragment

--- app/src/main/java/com/louis/app/cavity/ui/home/FragmentHome.kt
+++ app/src/main/java/com/louis/app/cavity/ui/home/FragmentHome.kt
@@ -13,7 +13,6 @@ import androidx.core.view.isVisible
 import androidx.core.view.marginRight
 import androidx.core.view.updateMargins
 import androidx.core.view.updatePadding
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
